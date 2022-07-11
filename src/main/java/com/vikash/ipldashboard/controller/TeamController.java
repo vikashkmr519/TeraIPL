23b613d677f58b11263f846d6d9befa28da964dc
vikashkmr519 <vikashkmr519@gmail.com>
2022-07-11 23:29:04 +0530

    started with frontend

--- src/main/java/com/vikash/ipldashboard/controller/TeamController.java
+++ src/main/java/com/vikash/ipldashboard/controller/TeamController.java
@@ -1,6 +1,7 @@
 package com.vikash.ipldashboard.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.vikash.ipldashboard.model.Team;
 import com.vikash.ipldashboard.service.TeamService;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 	
 	@Autowired
