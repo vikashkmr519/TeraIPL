f4dbe460ecce7a762f2533ba0d4f3bd3bf1f00e9
vikashkmr519 <vikashkmr519@gmail.com>
2022-07-12 23:09:42 +0530

    TeamPage fetching data

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const MatchSmallCard = ({ match }) => {
+export const MatchSmallCard = ({ teamName, match }) => {
+  if (!match) return null
+  const otherTeam = teamName === match.team1 ? match.team2 : match.team1
+  const color = {
+    color: 'blue',
+  }
+  const otherTeamRoute = `/teams/${otherTeam}`
   return (
     <div className="MatchSmallCard">
-      <p>
+      <h3>
         {' '}
-        {match.team1} vs {match.team2}
+        vs <Link to={otherTeamRoute}>{otherTeam}</Link>
+      </h3>
+      <p style={color}>
+        {match.winningTeam} won by {match.margin} {match.wonBy}
       </p>
+      <hr></hr>
     </div>
   )
 }
